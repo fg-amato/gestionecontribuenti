63378aae9d72d708e4f646443baf9bd7cafb2cf3
flavio.g.amato <flavio.g.amato@gmail.com>
2022-03-18 15:53:51 +0100

    Primo commit

---
+++ src/main/java/it/prova/gestionecontribuenti/GestionecontribuentiApplication.java
@@ -0,0 +1,13 @@
+package it.prova.gestionecontribuenti;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionecontribuentiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionecontribuentiApplication.class, args);
+	}
+
+}

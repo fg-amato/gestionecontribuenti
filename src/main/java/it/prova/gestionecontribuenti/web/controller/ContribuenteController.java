8be257e06d5172e3495dfc2d82c87b2cfcdfff4b
flavio.g.amato <flavio.g.amato@gmail.com>
2022-03-20 02:06:35 +0100

    Ricerca contribuenti inserita

--- src/main/java/it/prova/gestionecontribuenti/web/controller/ContribuenteController.java
+++ src/main/java/it/prova/gestionecontribuenti/web/controller/ContribuenteController.java
@@ -7,11 +7,13 @@ import javax.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.ui.ModelMap;
 import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.servlet.ModelAndView;
 import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
@@ -59,5 +61,19 @@ public class ContribuenteController {
 
 		redirectAttrs.addFlashAttribute("successMessage", "Operazione eseguita correttamente");
 		return "redirect:/contribuente";
+	}
+
+	@PostMapping("/list")
+	public String listContribuenti(ContribuenteDTO contribuenteExample,
+			@RequestParam(defaultValue = "0") Integer pageNo, @RequestParam(defaultValue = "9") Integer pageSize,
+			@RequestParam(defaultValue = "id") String sortBy, ModelMap model) {
+
+		List<Contribuente> contribuenti = contribuenteService
+				.findByExampleWithPagination(contribuenteExample.buildContribuenteModel(), pageNo, pageSize, sortBy)
+				.getContent();
+
+		model.addAttribute("contribuenti_list_attribute",
+				ContribuenteDTO.createContribuenteDTOListFromModelList(contribuenti));
+		return "contribuente/list";
 	}
 }
